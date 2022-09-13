2f3bd629a055bdefc31b82176e6ca658910cb240
arizalpratama <arizalpratama01@gmail.com>
2022-09-13 08:14:42 +0700

    commit

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ input_count = input('How much do you want buy apple?: ')
 count = int(input_count)
 total_price = apple_price * count
 
-print('You will buy ' + str(count) + ' apple')
-print('The total price is ' + str(total_price) + ' dollar')
-
 if money > total_price:
     print('You will buy ' + str(count) + ' apple')
-    print('The total price is ' + str(total_price) + 'dollar')
+    print('The total price is ' + str(total_price) + ' dollar')
 elif money == total_price:
     print('You had buy ' + str(count) + ' apple')
     print('Your wallet is empty now')
